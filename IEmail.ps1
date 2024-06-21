ef71a8c8b0668aef22007123f144447f1e56afec
Mhd Alghosh <49556550+MohamadAlghish93@users.noreply.github.com>
2024-06-21 15:12:15 +0000

    fix: email send function

--- IEmail.ps1
+++ IEmail.ps1
@@ -2,30 +2,29 @@
 ## Developer by the Alghish
 ### Send Email class
 
-
-
-function Send-Email([string]$emailTo, [string]$attachmentpath, [string]$username, [string]$password, [string]$bodyEmail, [string]$subject, [string]$emailFrom, [string]$emailHost, [string]$port, [string]$domain) {
+function Send-Email($emailTo, $attachmentpath, $username, $password, $bodyEmail, $subject, $emailFrom, $emailHost, $port, $domain) {
 
     Try {
         $DateYYYYMM = (Get-Date).ToString("yyyyMMdd")
         $message = new-object Net.Mail.MailMessage;
         $message.From = $emailFrom;
-        $message.To.Add($email);
-        $message.Subject = subject;
-        $message.Body = bodyEmail;
+        $message.To.Add($emailTo);
+        $message.Subject = $subject;
+        $message.Body = $bodyEmail;
         $attachment = New-Object Net.Mail.Attachment($attachmentpath);
         $message.Attachments.Add($attachment);
 
         $smtp = new-object Net.Mail.SmtpClient($emailHost, $port);
-        $smtp.EnableSSL = $true;
+        $smtp.EnableSSL = $false;
         $smtp.Credentials = New-Object System.Net.NetworkCredential($username, $password, $domain);
         $smtp.send($message);
 
-        Log-Message "Info" "Mail Sent $DateYYYYMM"
+        Log-Message "Info" "Mail $subject Sent $DateYYYYMM"
         $attachment.Dispose();
     }
     Catch {
-        Log-Message "Error: $($_.Exception.Message)" "Error"         
+        Log-Message "Error Email: $($_.Exception.Message)" "Error"         
     }
 
 }
+
